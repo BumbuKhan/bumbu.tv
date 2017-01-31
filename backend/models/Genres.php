fbd7c9603035f7249e6fcd6c4a17e52f9f024640
Курбан Курбанов <qurban.qranov93@gmail.com>
2017-01-31 09:27:52 +0400

    BACKEND - Added genres CRUD

---
+++ backend/models/Genres.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace backend\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "genres".
+ *
+ * @property integer $id
+ * @property string $title
+ */
+class Genres extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'genres';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['title'], 'required'],
+            [['title'], 'string', 'max' => 40],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'title' => 'Title',
+        ];
+    }
+}
